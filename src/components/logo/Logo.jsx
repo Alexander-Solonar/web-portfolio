dde3a9e122ebb5197d9272f65526b3e9d5e77e8d
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-09-03 19:19:42 +0300

    fix logo link

--- src/components/logo/Logo.jsx
+++ src/components/logo/Logo.jsx
@@ -3,7 +3,10 @@ import scss from "./Logo.module.scss";
 
 const Logo = () => {
   return (
-    <a href="/" className={scss.logo}>
+    <a
+      href="https://alexander-solonar.github.io/web-portfolio"
+      className={scss.logo}
+    >
       <svg width={140} height={36}>
         <use href={`${icons}#icon-logo`} />
       </svg>
