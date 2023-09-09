63b008beed42de59cbd750a3e0bb0786f2d34859
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-09-09 23:25:51 +0300

    changed link summary

--- src/components/sectionHero/SectionHero.jsx
+++ src/components/sectionHero/SectionHero.jsx
@@ -1,23 +1,23 @@
-import { useContext } from "react";
-import { Context } from "../../context/Context";
-import clsx from "clsx";
-import BtnConnection from "../btnConnection/BtnConnection";
-import image from "../../assets/images/hero-image.webp";
-import scss from "./SectionHero.module.scss";
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
+import clsx from 'clsx';
+import BtnConnection from '../btnConnection/BtnConnection';
+import image from '../../assets/images/hero-image.webp';
+import scss from './SectionHero.module.scss';
 
 const SectionHero = () => {
   const { theme } = useContext(Context);
 
   return (
-    <section className={clsx(scss.hero, theme && scss["hero-light"])}>
+    <section className={clsx(scss.hero, theme && scss['hero-light'])}>
       <div className="container">
         <div className={scss.content}>
-          <div className={scss["box-desc"]}>
-            <h3 className={scss["sub-title"]}>Hi!</h3>
+          <div className={scss['box-desc']}>
+            <h3 className={scss['sub-title']}>Hi!</h3>
             <h1 className={scss.title}>I’m Oleksandr Solonar</h1>
             <p className={scss.text}>
               I'm a Front-end developer and this is my Portfolio site. I have
-              been doing front-end development for over 17 months. Now I am
+              been doing front-end development for over 1.5 years. I'm currently
               looking for a job. If you have an open vacancy please contact me.
             </p>
 
--- src/components/sectionSummary/SectionSummary.jsx
+++ src/components/sectionSummary/SectionSummary.jsx
@@ -1,14 +1,14 @@
-import { useContext } from "react";
-import { Context } from "../../context/Context";
-import clsx from "clsx";
-import cv from "../../assets/images/cv-image.webp";
-import scss from "./SectionSummary.module.scss";
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
+import clsx from 'clsx';
+import cv from '../../assets/images/cv-image.webp';
+import scss from './SectionSummary.module.scss';
 
 const SectionSummary = () => {
   const { theme } = useContext(Context);
 
   return (
-    <section className={clsx(scss.summary, theme && scss["summary-light"])}>
+    <section className={clsx(scss.summary, theme && scss['summary-light'])}>
       <div className="container">
         <div className={scss.content}>
           <img className={scss.cv} src={cv} alt="" width={300} height={425} />
@@ -16,7 +16,7 @@ const SectionSummary = () => {
           <div className={scss.boxBtn}>
             <a
               className={scss.link}
-              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2FOleksandr_Solonar_Front-end%20Developer.pdf?alt=media&token=5beb269e-f971-4564-b89b-0edfc2c94606"
+              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2FOleksandr_Solonar_Front-end%20Developer.pdf?alt=media&token=7c3285b2-0c4b-4c37-b859-68bcbe4dcf87"
               target="_blank"
               rel="noopener noreferrer nofollow"
             >
@@ -24,7 +24,7 @@ const SectionSummary = () => {
             </a>
             <a
               className={scss.link}
-              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2F%D0%A1%D0%BE%D0%BB%D0%BE%D0%BD%D0%B0%D1%80%20%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80-Front-end%20Developer%20.pdf?alt=media&token=a7529704-7ea5-4814-b439-5bce20f21c55"
+              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2F%D0%A1%D0%BE%D0%BB%D0%BE%D0%BD%D0%B0%D1%80%20%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%20Front-end%20%D1%80%D0%BE%D0%B7%D1%80%D0%BE%D0%B1%D0%BD%D0%B8%D0%BA.pdf?alt=media&token=66ca5ff6-9faa-4a58-8cc7-c6377f0640ba"
               target="_blank"
               rel="noopener noreferrer nofollow"
             >
