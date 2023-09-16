f1e32978a3fb4ae67d52c71f38b29cd9cc6335ab
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-09-16 18:58:55 +0300

    fixed btntheme

--- src/components/btnTheme/BtnTheme.jsx
+++ src/components/btnTheme/BtnTheme.jsx
@@ -1,30 +1,28 @@
-import { useContext } from "react";
-import { Context } from "../../context/Context";
-import icons from "../../assets/icons/icons.svg";
-import scss from "./BtnTheme.module.scss";
+import { useContext } from 'react';
+import { Context } from '../../context/Context';
+import icons from '../../assets/icons/icons.svg';
+import scss from './BtnTheme.module.scss';
 
 const BtnTheme = () => {
   const { setTheme } = useContext(Context);
   const { theme } = useContext(Context);
 
-  const handleCheckboxChange = (event) => {
+  const handleCheckboxChange = event => {
     setTheme(event.target.checked);
   };
 
   return (
     <label className={scss.switch}>
-      <svg className={scss["icon-moon"]} width="16" height="16">
+      <svg className={scss['icon-moon']} width="16" height="16">
         <use href={`${icons}#icon-moon`}></use>
       </svg>
-      <label className={scss["label-switch-input"]}>
-        <input
-          type="checkbox"
-          checked={theme}
-          className={scss["switch-input"]}
-          onChange={handleCheckboxChange}
-        />
-      </label>
-      <svg className={scss["icon-sun"]} width="16" height="16">
+      <input
+        type="checkbox"
+        checked={theme}
+        className={scss['switch-input']}
+        onChange={handleCheckboxChange}
+      />
+      <svg className={scss['icon-sun']} width="16" height="16">
         <use href={`${icons}#icon-sun`}></use>
       </svg>
     </label>
