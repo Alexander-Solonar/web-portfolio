4fff55dec300da016bba64cb72908aedc7eaa267
Alexander-Solonar <Alexander-Solonar_solo991@ukr.net>
2023-11-23 16:31:04 +0200

    changed link cv

--- src/components/sectionSummary/SectionSummary.jsx
+++ src/components/sectionSummary/SectionSummary.jsx
@@ -18,7 +18,7 @@ const SectionSummary = () => {
           <div className={scss.boxBtn}>
             <a
               className={scss.link}
-              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2FOleksandr_Solonar_Front-end%20Developer.pdf?alt=media&token=7c3285b2-0c4b-4c37-b859-68bcbe4dcf87"
+              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2FOleksandr_Solonar_Front-end%20Developer.pdf?alt=media&token=689775c4-498b-4c2d-8d4d-1465f50dcf97"
               target="_blank"
               rel="noopener noreferrer nofollow"
             >
@@ -26,7 +26,7 @@ const SectionSummary = () => {
             </a>
             <a
               className={scss.link}
-              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2F%D0%A1%D0%BE%D0%BB%D0%BE%D0%BD%D0%B0%D1%80%20%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%20Front-end%20%D1%80%D0%BE%D0%B7%D1%80%D0%BE%D0%B1%D0%BD%D0%B8%D0%BA.pdf?alt=media&token=66ca5ff6-9faa-4a58-8cc7-c6377f0640ba"
+              href="https://firebasestorage.googleapis.com/v0/b/my-portfolio-c3ff0.appspot.com/o/sammury%2F%D0%A1%D0%BE%D0%BB%D0%BE%D0%BD%D0%B0%D1%80%20%D0%9E%D0%BB%D0%B5%D0%BA%D1%81%D0%B0%D0%BD%D0%B4%D1%80%20Front-end%20%D1%80%D0%BE%D0%B7%D1%80%D0%BE%D0%B1%D0%BD%D0%B8%D0%BA.pdf?alt=media&token=b045247c-fb9f-4099-bf5a-152ab38391d9"
               target="_blank"
               rel="noopener noreferrer nofollow"
             >
